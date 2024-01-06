fb94bbe0a1cb6bf754379af4275f8c828371144b
Morad17 <morad123@hotmail.co.uk>
2024-01-06 13:41:27 +0900

    initial commit

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# React Template
+
+Simple React Template for making projects
+
+## Modules Used
+
+* React Router In frontend providing routes
+
+* Main styling sheets is scss, in assets folder
---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Outlet,createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Footer from "./components/Footer"
+
+function App() {
+
+  const Layout = () => {
+    return (<div className="main-layout">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>)
+  }
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout/>,
+      children: [{
+        path: "/",
+        element: <Home />
+      },]
+    }
+  ])
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
